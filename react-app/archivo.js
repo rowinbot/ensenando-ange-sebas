f5fd4a48f2273d65c3a7d9ca9e155f376e392ea2
comangelo <acomuniello@gmail.com>
2019-12-30 13:18:34 +0000

    actualizacion

--- react-app/archivo.js
+++ react-app/archivo.js
@@ -1,17 +1,24 @@
-function Contador() {
-  var contador = React.useState(0);
+function Contador({ reset = "Reseteo", id }) {
+  // [0, ()  => {}]
+  var [valor, cambiarValor] = React.useState(0);
   var h = React.createElement;
 
-  return h("div", {}, [
-    h("button", { onClick: () => contador[1](contador[0] - 1) }, "Reducir"),
-    h("h1", {}, contador[0]),
-    h("button", { onClick: () => contador[1](contador[0] + 1) }, "Aumentar")
+  return h("div", { className: "contenedor", id: id }, [
+    h("button", { onClick: () => cambiarValor(valor - 1) }, "Reducir"),
+    h("div", { className: "centro" }, [
+      h("h1", {}, valor),
+      h("button", { id: "reset", onClick: () => cambiarValor(0) }, reset)
+    ]),
+    h("button", { onClick: () => cambiarValor(valor + 1) }, "Aumentar")
   ]);
 }
 
 window.onload = function() {
   ReactDOM.render(
-    React.createElement(Contador),
+    [
+      React.createElement(Contador, { reset: "Resetear", id: "contador-1" }),
+      React.createElement(Contador, { reset: "Resetear", id: "contador-2" })
+    ],
     document.getElementById("app")
   );
 };
